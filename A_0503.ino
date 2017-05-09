74206b6e2dd07ab748a3543fc194279ed2d5a348
ameng850127 <mengxuan850127@gmail.com>
2017-05-09 21:25:04 +0800

    123

---
+++ A_0503.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);// turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);// turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
